1bdc558ab7d232e850272005a41a4e9e016412d3
parameshwarrao <parameshwarrao.s@gmail.com>
2023-06-26 04:37:41 +0530

    update implementation

--- src/app/list/create-employee/create-employee.component.ts
+++ src/app/list/create-employee/create-employee.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { EmployeeService } from '../../shared/service/employee.service';
-import { createEmployeeReqbody } from '../../shared/models/employee.model';
+import { createEmployeeReqbody, employeeObject, updateEmployeeReqBody } from '../../shared/models/employee.model';
+
 
 @Component({
-  selector: 'app-create-employee',
+  selector: 'create-employee',
   templateUrl: './create-employee.component.html',
   styleUrls: ['./create-employee.component.scss']
 })
@@ -14,7 +15,14 @@ export class CreateEmployeeComponent implements OnInit {
   public errorMessage: string = "";
   createEmployeeForm: FormGroup = new FormGroup({});
   isError: boolean = false;
-  updatedInfo:string = "";
+  updatedInfo: string = "";
+
+  @Input() public isEdit: boolean = false;
+  @Output() public recordUpdated: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  //edit
+  public editEmployee: updateEmployeeReqBody = new updateEmployeeReqBody();
+  public editEmployeeID:string ="";
 
   public createEmployeeReq: createEmployeeReqbody = new createEmployeeReqbody();
 
@@ -53,11 +61,63 @@ export class CreateEmployeeComponent implements OnInit {
     }
   }
 
-  createEmployee() {
+  /**
+   * Used only for edit via viewChild
+   * */
+  public updateFormForEdit(employeeObject: employeeObject) {
+    this.editEmployeeID = employeeObject._id;
+
+    let {
+      Hire_Date,
+      Annual_Salary,
+      Bonus
+    } = employeeObject;
+
+    let hiredate = Hire_Date ? new Date(`${Hire_Date}`) : "";
+    let annualSalary = employeeObject.Annual_Salary ?  employeeObject.Annual_Salary.split("$") : "";
+    let bonus = employeeObject.Bonus ?  employeeObject.Bonus.split("%") : "";
+
+    let hiredateVal = Hire_Date ?  Hire_Date : "";
+
+    var dateString = hiredateVal; // Oct 23
+
+var dateParts = dateString.split("/");
+//continue with date bug!!
+
+
+    let annualSalaryVal = annualSalary[1] ? parseFloat(`${annualSalary[1].trim()}`) : "";
+    let bonusVal = bonus[0] ? Number(`${bonus[0].trim()}`) : "";
+
+    
+
+    
+
+
+    this.createEmployeeForm = this._FormBuilder.group({
+      Employee_ID: [`${employeeObject.Employee_ID}`, Validators.required],
+      Full_Name: [`${employeeObject.Full_Name}`, Validators.required],
+      Job_Title: [`${employeeObject.Job_Title}`, Validators.required],
+      Department: [`${employeeObject.Department}`, Validators.required],
+      Business_Unit: [`${employeeObject.Business_Unit}`, Validators.required],
+      Gender: [`${employeeObject.Gender}`, Validators.required],
+      Ethnicity: [`${employeeObject.Ethnicity}`, Validators.required],
+      Age: [`${employeeObject.Age}`, Validators.required],
+      Hire_Date: [hiredateVal, Validators.required],
+      Annual_Salary: [annualSalaryVal, Validators.required],
+      Bonus: [bonusVal, Validators.required],
+      Country: [`${employeeObject.Country}`, Validators.required],
+      City: [`${employeeObject.City}`, Validators.required]
+    });
+  }
+
+  createOrUpdateEmployee() {
     this.createEmployeeForm.markAllAsTouched();
+
     if (this.createEmployeeForm.valid) {
       //api call
       console.log(this.createEmployeeForm.value);
+
+
 
       let { Employee_ID,
         Full_Name,
@@ -73,47 +133,95 @@ export class CreateEmployeeComponent implements OnInit {
         Country,
         City } = this.createEmployeeForm.value;
 
-      this.createEmployeeReq = new createEmployeeReqbody();
+      if (this.isEdit) {
 
-      this.createEmployeeReq = {
-        Employee_ID: `${Employee_ID}`,
-        Full_Name: `${Full_Name}`,
-        Job_Title: `${Job_Title}`,
-        Department: `${Department}`,
-        Business_Unit: `${Business_Unit}`,
-        Gender: `${Gender}`,
-        Ethnicity: `${Ethnicity}`,
-        Age: `${Age}`,
-        Hire_Date: `${Hire_Date}`,
-        Annual_Salary: `${Annual_Salary}$`,
-        Bonus: `${Bonus}%`,
-        Country: `${Country}`,
-        City: `${City}`
-      };
-
-      //api call
-      this._EmployeeService.createEmployee(this.createEmployeeReq).subscribe({
-        next: (data) => {            
-          this.ScrollToTop();
-          this.updatedInfo = "Employee Record has been created!";
-          this.resetEmployee();
-          setTimeout(() => {
-            this._router.navigate(['/List']);
-          },1200);
-
-        },
-        error: (err) => {
-          this.ScrollToTop();
-          this.isError = true;
-          let { error } = err;
-          if (error && error.errors) {
-            this.errorMessage = error.errors[0] ? error.errors[0].msg : "some error";
-          }
-          else{
-            this.errorMessage = "some error";
-          }
+        this.editEmployee ={
+          _id:`${this.editEmployeeID}`,
+          Employee_ID: `${Employee_ID}`,
+          Full_Name: `${Full_Name}`,
+          Job_Title: `${Job_Title}`,
+          Department: `${Department}`,
+          Business_Unit: `${Business_Unit}`,
+          Gender: `${Gender}`,
+          Ethnicity: `${Ethnicity}`,
+          Age: `${Age}`,
+          Hire_Date: `${Hire_Date}`,
+          Annual_Salary: `$`+`${Annual_Salary}`,
+          Bonus: `${Bonus}%`,
+          Country: `${Country}`,
+          City: `${City}`
         }
-      });
+
+        //api call
+        this._EmployeeService.updateEmployee(this.editEmployee).subscribe({
+          next: (data) => {
+            this.ScrollToTop();
+            this.updatedInfo = "Employee Record has updated!";
+            this.resetEmployee();
+            setTimeout(() => {
+              this.recordUpdated.emit(true);
+            }, 1200);
+
+          },
+          error: (err) => {
+            this.ScrollToTop();
+            this.isError = true;
+            let { error } = err;
+            if (error && error.errors) {
+              this.errorMessage = error.errors[0] ? error.errors[0].msg : "some error";
+            }
+            else {
+              this.errorMessage = "some error";
+            }
+          }
+        });
+
+      }
+      else {
+
+        this.createEmployeeReq = new createEmployeeReqbody();
+
+        this.createEmployeeReq = {
+          Employee_ID: `${Employee_ID}`,
+          Full_Name: `${Full_Name}`,
+          Job_Title: `${Job_Title}`,
+          Department: `${Department}`,
+          Business_Unit: `${Business_Unit}`,
+          Gender: `${Gender}`,
+          Ethnicity: `${Ethnicity}`,
+          Age: `${Age}`,
+          Hire_Date: `${Hire_Date}`,
+          Annual_Salary: `$`+`${Annual_Salary}`,
+          Bonus: `${Bonus}%`,
+          Country: `${Country}`,
+          City: `${City}`
+        };
+
+        //api call
+        this._EmployeeService.createEmployee(this.createEmployeeReq).subscribe({
+          next: (data) => {
+            this.ScrollToTop();
+            this.updatedInfo = "Employee Record has been created!";
+            this.resetEmployee();
+            setTimeout(() => {
+              this._router.navigate(['/List']);
+            }, 1200);
+
+          },
+          error: (err) => {
+            this.ScrollToTop();
+            this.isError = true;
+            let { error } = err;
+            if (error && error.errors) {
+              this.errorMessage = error.errors[0] ? error.errors[0].msg : "some error";
+            }
+            else {
+              this.errorMessage = "some error";
+            }
+          }
+        });
+
+      }
 
 
 
@@ -122,14 +230,17 @@ export class CreateEmployeeComponent implements OnInit {
   }
   resetEmployee() {
     this.createEmployeeForm.reset();
+    if(this.isEdit){
+      this.recordUpdated.emit(true);
+    }
   }
 
   ScrollToTop() {
-		window.scrollTo({
-			top: 0,
-			left: 0,
-			behavior: 'smooth',
-		});
-	}
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
 
 }
--- src/app/list/employee-detail/employee-detail.component.ts
+++ src/app/list/employee-detail/employee-detail.component.ts
@@ -1,8 +1,11 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { EmployeeService } from '../../shared/service/employee.service';
-import { detailResponse } from '../../shared/models/employee.model';
+import { detailResponse, employeeObject } from '../../shared/models/employee.model';
+import { CreateEmployeeComponent } from '../create-employee/create-employee.component';
+import { FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-employee-detail',
@@ -10,17 +13,28 @@ import { detailResponse } from '../../shared/models/employee.model';
   styleUrls: ['./employee-detail.component.scss']
 })
 export class EmployeeDetailComponent implements OnInit, AfterViewInit {
-public isError:boolean = false;
+  public isError: boolean = false;
+
+  public enableEdit: boolean = false;
+
+  public id: string = "";
+
+  @ViewChild('createEmpRef', { static: false }) _createEmployeeRef: CreateEmployeeComponent = new CreateEmployeeComponent(this._FormBuilder,
+    this._router,
+    this._EmployeeService);
+
   employeeDetail: detailResponse = new detailResponse();
   constructor(private _activatedRoute: ActivatedRoute,
     private _router: Router,
     private _location: Location,
-    private _EmployeeService: EmployeeService) {
+    private _EmployeeService: EmployeeService,
+    private _FormBuilder: FormBuilder) {
 
   }
   ngAfterViewInit(): void {
     this._activatedRoute.queryParams.subscribe((params) => {
       if (params && params['id']) {
+        this.id = params['id'];
         this.getEmployeeDetails(params['id']);
       }
     });
@@ -45,12 +59,23 @@ public isError:boolean = false;
     this._location.back();
   }
 
-  deleteEmployee(){
+  deleteEmployee() {
 
   }
 
-  updateEmployee(){
+  updateEmployee() {
+    this.enableEdit = true;
+    let employeeDat = <employeeObject>this.employeeDetail;
+    setTimeout(() => {
+      this._createEmployeeRef.updateFormForEdit(employeeDat);
+    });
     
+  }
+
+  updateEmployeeHandler($event: any) {
+    this.enableEdit = false;
+    this.getEmployeeDetails(this.id);
+
   }
 
 }
